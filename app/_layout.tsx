97d9a0fa6099ee75da1e7026a3bc1de18102475d
Satyam-Maru <satyammaru07@gmail.com>
2026-04-04 00:47:42 +0530

    added custom splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,46 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import LottieView from "lottie-react-native";
+import { useCallback, useState } from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  return <Stack />;
+  const [animationFinished, setAnimationFinished] = useState(false);
+
+  const onLayoutRootView = useCallback(async () => {
+    await SplashScreen.hideAsync()
+  }, []);
+
+  if (animationFinished) {
+    return <Stack screenOptions={{ headerShown: false }} />;
+  }
+
+  return (
+    <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+      <LottieView
+        source={require("../assets/lottie-animation/Tree.json")}
+        autoPlay
+        loop={false}
+        resizeMode="center"
+        onAnimationFinish={() => setAnimationFinished(true)}
+        style={styles.lottie}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  lottie: {
+    width: "100%",
+    height: "100%",
+  },
+});
